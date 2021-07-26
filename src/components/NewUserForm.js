7bad4f1b07dfa8c8cc8a233e49af6a21d3e6f9de
Gedoelba2al <ioegy@outlook.com>
2021-07-26 13:12:29 +0200

    Form validation

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { checkForMobNum } from './validation';
+import {
+  checkForMobNum,
+  checkForEmail,
+  checkForUserName,
+  checkForMembership,
+} from './validation';
 
 class NewUserForm extends React.Component {
   state = {
@@ -12,10 +17,26 @@ class NewUserForm extends React.Component {
 
   onNewUserFormSubmit = async (event) => {
     event.preventDefault();
-    const validMobile = await checkForMobNum(this.state.mobile);
-    this.setState({ validate: { mobile: validMobile } });
-    if (this.state.validate.mobile === false) return;
 
+    const validMobile = await checkForMobNum(this.state.mobile);
+    const validEmail = await checkForEmail(this.state.email);
+    const validUserName = await checkForUserName(this.state.userName);
+    const validMembership = await checkForMembership(this.state.membership);
+
+    this.setState({
+      validate: {
+        mobile: validMobile,
+        email: validEmail,
+        userName: validUserName,
+        membership: validMembership,
+      },
+    });
+
+    if (this.state.validate.mobile === false) return;
+    if (this.state.validate.email === false) return;
+    if (this.state.validate.userName === false) return;
+    if (this.state.validate.membership === false) return;
+    
     this.props.onSubmit({
       userName: this.state.userName,
       mobile: this.state.mobile,
@@ -48,6 +69,14 @@ class NewUserForm extends React.Component {
               name='userName'
               onChange={this.myChangeHandler}
             />
+            {this.state.validate.userName === null ||
+            this.state.validate.userName === true ? (
+              ''
+            ) : (
+              <div className='alert alert-danger' role='alert'>
+                Enter valid User Name
+              </div>
+            )}
             <label>Mobile Number</label>
             <input
               type='text'
@@ -65,6 +94,14 @@ class NewUserForm extends React.Component {
             )}
             <label>E-Mail Address</label>
             <input type='text' name='email' onChange={this.myChangeHandler} />
+            {this.state.validate.email === null ||
+            this.state.validate.email === true ? (
+              ''
+            ) : (
+              <div className='alert alert-danger' role='alert'>
+                Enter valid E-Mail Address
+              </div>
+            )}
             <label>
               Membership
               <select name='membership' onChange={this.myChangeHandler}>
@@ -77,6 +114,14 @@ class NewUserForm extends React.Component {
                 <option value='Hours'>Hours</option>
               </select>
             </label>
+            {this.state.validate.membership === null ||
+            this.state.validate.membership === true ? (
+              ''
+            ) : (
+              <div className='alert alert-danger' role='alert'>
+                Select Membership
+              </div>
+            )}
 
             <button
               className='ui primary button stabraq-bg'
--- src/components/validation.js
+++ src/components/validation.js
@@ -11,3 +11,47 @@ export const checkForMobNum = async (inputTerm) => {
   }
   return false;
 };
+
+export const checkForEmail = async (inputTerm) => {
+  console.log('::: Running checkForEmail :::', inputTerm);
+
+  function validEmail(inputTerm) {
+    let regexp =
+      /^[\w]{1,}[\w.+-]{0,}@[\w-]{2,}([.][a-zA-Z]{2,}|[.][\w-]{2,}[.][a-zA-Z]{2,})$/g;
+    return regexp.test(inputTerm);
+  }
+
+  if (validEmail(inputTerm)) {
+    return true;
+  }
+  return false;
+};
+
+export const checkForUserName = async (inputTerm) => {
+  console.log('::: Running checkForUserName :::', inputTerm);
+
+  function validUserName(inputTerm) {
+    let regexp =
+      /^[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z\s]+[\u0600-\u065F\u066A-\u06EF\u06FA-\u06FFa-zA-Z-_\s]*$/;
+    return regexp.test(inputTerm);
+  }
+
+  if (validUserName(inputTerm)) {
+    return true;
+  }
+  return false;
+};
+
+export const checkForMembership = async (inputTerm) => {
+  console.log('::: Running checkForMembership :::', inputTerm);
+
+  function validMembership(inputTerm) {
+    let regexp = /\w{1,}/;
+    return regexp.test(inputTerm);
+  }
+
+  if (validMembership(inputTerm)) {
+    return true;
+  }
+  return false;
+};
